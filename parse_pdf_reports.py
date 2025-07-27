55da0de82dfaab6246884c3a493aac3006bb6e96
Bill <10003477+constructor-s@users.noreply.github.com>
2025-07-27 00:36:00 -0400

    Organize output in Excel sheets

--- parse_pdf_reports.py
+++ parse_pdf_reports.py
@@ -4,24 +4,28 @@ import glob
 import pandas as pd
 
 # The first argument is a glob pattern to match PDF files.
-results = []
+from collections import defaultdict
+results = defaultdict(list)  # The key is title
 pdf_files = sorted(glob.glob(sys.argv[1]))
 for pdf_file in pdf_files:
     with IOLMasterPDFParser(pdf_file) as parser:
         pdf_data = parser.get_pdf_data()
-        results.append(pdf_data)
+        results[pdf_data["title"]].append(pdf_data)
 
 def flatten_dict(d, parent_key='', sep='_'):
     """Flatten a nested dictionary."""
-    items = []
+    items = {}
     for k, v in d.items():
         new_key = f"{parent_key}{sep}{k}" if parent_key else k
         if isinstance(v, dict):
-            items.extend(flatten_dict(v, new_key, sep=sep).items())
+            items.update(flatten_dict(v, new_key, sep=sep))
         else:
-            items.append((new_key, v))
-    return dict(items)
+            items[new_key] = v
+    return items
 
-df = pd.DataFrame([flatten_dict(data) for data in results])
-df = df.apply(pd.to_numeric, errors='ignore')
-df.to_excel("parsed_results.xlsx", index=False)
+# Write each type of parsed result to one sheet in an Excel file.
+with pd.ExcelWriter("parsed_results.xlsx") as writer:
+    for title, data in results.items():
+        df = pd.DataFrame([flatten_dict(d) for d in data])
+        df = df.apply(pd.to_numeric, errors='ignore')
+        df.to_excel(writer, sheet_name=title[:31], index=False)
